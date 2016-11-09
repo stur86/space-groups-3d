565ffb42cbe7c930dd91eea59d2900baf23c4df1
stur86 <simonesturniolo@gmail.com>
2016-11-09 20:07:25 +0000

    Added JSON symmetry data

--- js/lattice.js
+++ js/lattice.js
@@ -1,12 +1,23 @@
 // NPM imports
 var THREE = require('three');
+var TWEEN = require('tween.js');
+
+var SYMDATA = require('./symgroup_data.json');
 
 var lsize = 12; // Default starting point
 // Actual scene-space size will be 1
 
+// Other stuff
+var anim_t = 500;
+var base_scale = 0.01;
+var scale_fac = 5;
+
 // Lattice parameters
 var lattice_abc = [[1, 1, 2], [90, 90, 120]]; // Starting with a cube
 var lattice_cart;
+// Symmetry group
+var current_group = 'P -1';
+
 // Cell box
 var boxGeom, boxMat, boxMesh;
 
@@ -15,7 +26,12 @@ var pGroup;
 var textureLoader = new THREE.TextureLoader();
 var pointTex = textureLoader.load("img/point.png");
 var pointMat = new THREE.SpriteMaterial( { map: pointTex, color: 0xffffff, fog: true });
-var pointArr;
+var pointArr; // Array of tweens
+
+function latticePoint(sprite) {
+    this.sprite = sprite;
+    this.tween = null;
+}
 
 function computeCartesian() {
     // Computes Cartesian lattice from abc parameters
@@ -74,6 +90,23 @@ function computeGeom() {
     boxGeom = new THREE.EdgesGeometry(boxGeom); // Convert to wireframe
 }
 
+function threefySymData() {
+
+    for (var group in SYMDATA) {
+        // Convert the operators to THREE.js format
+        var ops = SYMDATA[group].ops;
+        for (var i = 0; i < ops.length; ++i) {
+            var mat_arr = ops[i][0];
+            ops[i][0] = new THREE.Matrix3();
+            ops[i][0].fromArray(mat_arr);
+            ops[i][1] = new THREE.Vector3(ops[i][1][0],
+                                          ops[i][1][1],
+                                          ops[i][1][2]);
+        }
+        SYMDATA.ops = ops;
+    }
+}
+
 function renderBox() {
 
     boxMat = new THREE.LineBasicMaterial( { color: 0xaaaaaa, linewidth: 2} );
@@ -83,11 +116,34 @@ function renderBox() {
 
 }
 
+function visualiseSymmetry(xyz) {
+    var x0 = xyz[0];
+    var y0 = xyz[1];
+    var z0 = xyz[2];
+
+    var sprite = pointArr[x0][y0][z0].sprite;
+
+    var tween = new TWEEN.Tween({s: base_scale})
+                    .to({s: base_scale*scale_fac}, anim_t)
+                    .easing(TWEEN.Easing.Elastic.InOut)
+                    .onUpdate( function () {
+                        sprite.scale.set(this.s, this.s, this.s);
+                    })
+                    .chain(new TWEEN.Tween({s: base_scale*scale_fac})
+                        .to({s: base_scale}, anim_t)
+                        .easing(TWEEN.Easing.Elastic.InOut)
+                        .onUpdate( function () {
+                            sprite.scale.set(this.s, this.s, this.s);
+                        }))
+                    .start();
+}
+
 exports.init = function() {
 
     computeCartesian();
     computeGeom();
     renderBox();
+    threefySymData();
 
     group = new THREE.Group();
     // Initialise the lattice of points
@@ -106,13 +162,20 @@ exports.init = function() {
                 //mat.color.setRGB( (x*0.5) +0.5, (y*0.5) +0.5, (z*0.5)+0.5);
                 var sprite = new THREE.Sprite( mat );
                 sprite.position.set(fp.x, fp.y, fp.z);
-                sprite.scale.set(0.01, 0.01, 0.01);
-                Renderer.addObject(sprite);
+                sprite.scale.set(base_scale, base_scale, base_scale);
+                sprite.data_index = [x,y,z];
+                group.add(sprite);
 
-                pointArr[x][y].push(sprite);
-            }   
+                pointArr[x][y].push(new latticePoint(sprite));
+            }
         }        
     }
 
+    Renderer.addObject(group);
+    Renderer.addClickListener(function(i) {
+        if (i.length < 1)
+            return;
+        visualiseSymmetry(i[0].object.data_index);
+    }, group);
 }
 
--- js/render.js
+++ js/render.js
@@ -1,6 +1,7 @@
 // NPM imports
 var $ = require('jquery');
 var THREE = require('three');
+var TWEEN = require('tween.js');
 var OrbitControls = require('three-orbit-controls')(THREE);
 
 var width;
@@ -8,6 +9,8 @@ var height;
 
 var appdiv;
 var camera, scene, renderer;
+
+var ray_lstn = [];
 
 function updateSize() {
     width = appdiv.innerWidth();
@@ -24,10 +27,36 @@ function onWindowResize() {
     renderer.setSize(width, height);
 }
 
+function onClickRaycast(e) {
+    //We create a 2D vector
+    vector = new THREE.Vector2();
+    //We set its position where the user clicked and we convert it to a number between -1 & 1
+    vector.set(
+        2*(e.clientX/width)-1,
+        1-2*(e.clientY/height)
+    );
+
+    //We create a raycaster, which is some kind of laser going through your scene
+    raycaster = new THREE.Raycaster();
+    //We apply two parameters to the 'laser', its origin (where the user clicked) and the direction (what the camera 'sees')
+    raycaster.setFromCamera(vector,camera);
+
+    //We get all the objects the 'laser' find on its way (it returns an array containing the objects)
+    for (var i = 0; i < ray_lstn.length; ++i) {
+
+        var targ = ray_lstn[i][1] || scene;
+
+        intersects = raycaster.intersectObjects(targ.children);
+        ray_lstn[i][0](intersects);
+    }
+}
+
 // Initialise renderer
 exports.init = function() {
 
     appdiv = $('.main-app-content');
+
+    ray_lstn = [];
 
     // Get the size
     updateSize();
@@ -48,9 +77,12 @@ exports.init = function() {
     controls = new OrbitControls( camera, renderer.domElement );
     controls.addEventListener( 'change', exports.render );
 
-    $('.main-app-content').append(renderer.domElement);
+    appdiv.append(renderer.domElement);
 
     window.addEventListener( 'resize', onWindowResize, false );
+
+    // Add raycast hit event listener
+    renderer.domElement.addEventListener('mousedown', onClickRaycast);
 }
 
 exports.render = function() {
@@ -60,9 +92,17 @@ exports.render = function() {
 
 exports.animate = function() {
     requestAnimationFrame(exports.animate);
-    exports.render();
+    TWEEN.update();
+    exports.render();    
 }
 
 exports.addObject = function(o) {
     scene.add(o);
+}
+
+exports.addClickListener = function(listener, group) {
+
+    group = group || null;
+
+    ray_lstn.push([listener, group]);
 }
